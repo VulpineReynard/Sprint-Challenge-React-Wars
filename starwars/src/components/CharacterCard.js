46cfd2e284552df2cd7f096973cc452ce0ef0984
VulpineReynard <milare8@gmail.com>
2019-07-19 16:48:03 -0400

    wip: character cards - added styled-components for styling on card element

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import styled from "styled-components";
+
+const CharacterName = styled.h2`
+  text-align: center;
+  margin: 5px auto;
+  `;
+const CharacterHeight = styled.p`
+
+`;
+
+const CharacterBirthYear = styled.p`
+
+`;
+
+const CharacterEyeColor = styled.p`
+
+`;
+
+const CharacterFilms = styled.ul`
+
+`;
+
+const Film = styled.li`
+
+`;
 
 export default function CharacterCard({character}) {
   return (
     <div className="card">
-      <h2>{character.name}</h2>
+      <CharacterName>{character.name}</CharacterName>
+      <CharacterHeight>Height: {character.height} in.</CharacterHeight>
     </div>
   );
 }
