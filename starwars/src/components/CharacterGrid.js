c8c5c1e391b62944db76624db3320e4cb266ec74
VulpineReynard <milare8@gmail.com>
2019-07-19 16:09:58 -0400

    receiving correct data from api, building cards for each character

---
+++ starwars/src/components/CharacterGrid.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CharacterCard from "./CharacterCard";
+
+export default function CharacterGrid(props) {
+  return (
+    <div className="grid">
+      {
+        props.listOfCharacters.map((character, index) => {
+          return <CharacterCard
+          key={index}
+          name={character.name}
+          height={character.height}
+          birthYear={character.birth_year}
+          eyeColor={character.eye_color}
+          films={character.films}
+          homeworld={character.homeworld}
+          />
+        })
+      }
+    </div>
+  );
+}
